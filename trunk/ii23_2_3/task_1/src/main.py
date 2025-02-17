57aaf22c6cb7b8f403b02babbc11ac34ccebe877
Makarevich Nikita <129955309+sosiska52@users.noreply.github.com>
2025-02-17 14:41:09 +0300

    del

--- trunk/ii23_2_3/task_1/src/main.py
+++
@@ -1,9 +0,0 @@
-from app import ImageDenoiseApp
-from image_manipulation import add_noise_dot, add_noise_line, median_filter_1d
-import tkinter as tk
-from tkinter import filedialog
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = ImageDenoiseApp(root)
-    root.mainloop()
